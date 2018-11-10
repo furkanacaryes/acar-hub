78242d6dbb71ec888c200d3f32c192d4aa87299a
Furkan Acar <furkan@kimbukedi.com>
2018-11-10 13:09:28 +0300

    added mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mongoDB_URL = 'mongodb://nodejs_02:123qwe@ds052649.mlab.com:52649/nodejs_02';
+const session = mongoose.model('session', {
+    title: String,
+    message: String,
+    buttons: []
+})
 const port = process.env.PORT || 3000;
 
 
@@ -10,8 +17,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-server.listen(port, () => {
+server.listen(port, _ => {
     console.log('Server is Up!', port);
+})
+
+
+mongoose.connect(mongoDB_URL, {useNewUrlParser: true}, err => {
+    if(err)
+        console.log('MongoDB Couldn\'t Connect!')
+    
+    console.log('MongoDB Connection Established.')
 })
 
 
@@ -61,13 +76,22 @@ notifications.on('connection', socket => {
         socket.join(room);
         console.log(`New Client Joined and Encapsulated as ${room}`);
 
-        // Informs Admin Client to make changes on UI
-
-        notifications.emit('session', {
+        const newSession = {
             user: room,
             client: Object.keys(socket.rooms).join(' '),
             timeStamp: new Date()
-        })
+        };
+
+        // Keep persistent
+
+        session.create(newSession, err => {
+            console.log('Session Couldn\'t Save!')
+        });
+
+
+        // Informs Admin Client to make changes on UI
+
+        notifications.emit('session', newSession)
     })
 
 
@@ -89,6 +113,12 @@ notifications.on('connection', socket => {
 
 
     socket.on('disconnecting', _ => {
+        session.findOneAndDelete({
+            client: Object.keys(socket.rooms).join(' ')},
+            err => {
+                if(err)
+                    console.log('Couldn\'t Delete!', err)
+            })
 
         // Informs Admin Client to make changes on UI
 
